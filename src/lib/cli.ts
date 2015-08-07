03ed3a63f13d86f28ab8de59f5e110d2ab727581
motemen <motemen@gmail.com>
2015-08-07 23:16:59 +0900

    promise

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -13,16 +13,99 @@ class Command {
   out: string = '';
   err: string = '';
 
+  exitCode: number;
+
   constructor(cmd: string[]) {
     this.command = cmd[0];
     this.args    = cmd.slice(1);
   }
 
-  run(cb: (number) => void) {
-    var p = this.process = child_process.spawn(this.command, this.args);
-    p.stdout.on('data', (data) => { this.out += data });
-    p.stderr.on('data', (data) => { this.err += data });
-    p.on('close', (code: number) => cb(code));
+  run(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.process = child_process.spawn(this.command, this.args);
+      this.process.stdout.on('data', (data: string) => { this.out += data });
+      this.process.stderr.on('data', (data: string) => { this.err += data });
+      this.process.on('close', (exitCode: number) => {
+        this.exitCode = exitCode;
+        resolve();
+      });
+    });
+  }
+}
+
+interface RevenantOpts {
+  maxAttempts?: number;
+  timeoutSeconds?: number;
+  outputPatterns?: RegExp[];
+  verbose?: boolean;
+}
+
+class Revenant implements RevenantOpts {
+  // The command to invoke and retry on failure.
+  commandArgs: string[];
+
+  // Maximum number of retries.
+  maxAttempts: number = 5;
+
+  // If the process took longer than this duration, kill it and retry.
+  timeoutSeconds: number = 0;
+
+  // If set, retry only if the patterns are met to the stdout/stderr.
+  outputPatterns: RegExp[] = [];
+
+  verbose: boolean = false;
+
+  attempts: number = 0;
+
+  constructor (commandArgs: string[], opts?: RevenantOpts) {
+    this.commandArgs = commandArgs;
+
+    if (opts) {
+      this.maxAttempts    = opts.maxAttempts;
+      this.timeoutSeconds = opts.timeoutSeconds;
+      this.outputPatterns = opts.outputPatterns;
+      this.verbose        = opts.verbose;
+    }
+
+    this.maxAttempts = this.maxAttempts || 5;
+  }
+
+  run(): Promise<Command> {
+    this.log('--> run ' + JSON.stringify(this.commandArgs));
+
+    var next: () => Promise<Command> = () => {
+      this.attempts++;
+
+      var cmd = new Command(this.commandArgs);
+
+      return cmd.run()
+        .then(() => {
+          if (cmd.exitCode === 0 || this.attempts >= this.maxAttempts) {
+            return cmd;
+          }
+
+          this.log(`--> exit=${cmd.exitCode} attempts=${this.attempts}/${this.maxAttempts}`);
+          this.log(cmd.out, { prefix: 'OUT ' });
+          this.log(cmd.err, { prefix: 'ERR ' });
+
+          return next();
+        });
+    }
+
+    return next();
+  }
+
+  private log(data: string, opts?: { prefix?: string; }) {
+    var lines = data.split(/\n/);
+    if (lines[lines.length-1] === '') {
+      lines.pop()
+    }
+    lines.forEach((line: string) => {
+      if (opts && opts.prefix) {
+        line = opts.prefix + line;
+      }
+      process.stderr.write(`# [${this.attempts}/${this.maxAttempts}] ${line}\n`);
+    });
   }
 }
 
@@ -33,65 +116,24 @@ if (opts._.length === 0) {
   process.exit(1);
 }
 
-_log('--> run ' + JSON.stringify(opts._))
+var revOpts: RevenantOpts = {};
 
-var maxRetries: number   = 0+opts['c'] || 5;
-var timeout   : number   = 0+opts['t'];
-var patterns  : RegExp[] = (opts['p'] instanceof Array ? opts['p'] : [ opts['p'] ]).map((p) => new RegExp(p));
-
-function retry1(args: string[], remains: number) {
-  var cmd = new Command(args);
-
-  var timer: NodeJS.Timer;
-
-  cmd.run((exitCode: number) => {
-    clearTimeout(timer);
-
-    var finish: boolean = false;
-
-    if (exitCode === 0 || remains <= 0) {
-      finish = true;
-    }
-
-    if (patterns.length) {
-      if (patterns.every((p) => !p.exec(cmd.out) && !p.exec(cmd.err))) {
-        // if -p was given and none matched, give up.
-        finish = true
-      }
-    }
-
-    if (finish) {
-      process.stdout.write(cmd.out);
-      process.stderr.write(cmd.err);
-      process.exit(exitCode);
-    }
-
-    _log(`--> exit=${exitCode} remains=${remains}/${maxRetries}`);
-    _log(cmd.out, { prefix: 'OUT' });
-    _log(cmd.err, { prefix: 'ERR' });
-
-    retry1(args, remains-1);
-  });
-
-  if (timeout > 0) {
-    timer = setTimeout(function () {
-      _log(`--> timed out after ${timeout * 1000}ms`);
-      cmd.process.kill();
-    }, timeout * 1000);
-  }
+if ('c' in opts) {
+  revOpts.maxAttempts = 0+opts['c'];
 }
 
-retry1(opts._, maxRetries);
-
-function _log(data: string, opts?: { prefix: string; }) {
-  var lines = data.split(/\n/);
-  if (lines[lines.length-1] === '') {
-    lines.pop()
-  }
-  lines.forEach((line: string) => {
-    if (opts && opts.prefix) {
-      line = `${opts.prefix} ${line}`
-    }
-    process.stderr.write(`# [revenant] ${line}\n`);
-  });
+if ('t' in opts) {
+  revOpts.timeoutSeconds = 0+opts['t'];
 }
+
+if ('p' in opts) {
+  revOpts.outputPatterns = (opts['p'] instanceof Array ? opts['p'] : [ opts['p'] ])
+    .map((p: string) => new RegExp(p));
+}
+
+var app = new Revenant(opts._, revOpts);
+app.run().then((cmd: Command) => {
+  process.stdout.write(cmd.out);
+  process.stderr.write(cmd.err);
+  process.exit(cmd.exitCode);
+});
